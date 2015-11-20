4c2cd887b6ff3864d51f6f68eed41375a0234847
Gabriel <Gabriel@DESKTOP-MVSO4F3>
2015-11-20 00:40:53 -0200

    teste em venda

--- src/LojaVirtual/Venda.java
+++ src/LojaVirtual/Venda.java
@@ -26,5 +26,9 @@ public class Venda extends Operacao {
     public boolean isDevolvido() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    public void teste() {
+        System.out.println("teste");
+    }
 
 }
